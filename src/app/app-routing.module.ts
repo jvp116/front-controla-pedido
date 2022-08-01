302066b77449a746f32ac1c6e69fa303fcafaa91
Joao VP <joaovpaltanin116@gmail.com>
2022-08-01 10:47:53 -0300

    estruturando projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
+import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cliente', component: CadastroClienteComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
