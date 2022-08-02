ca2b31f49cf20178c87acfe118b49344f330ae6d
Joao VP <joaovpaltanin116@gmail.com>
2022-08-02 13:22:18 -0300

    objeto para receber a chamada do listagem de clientes

---
+++ src/app/shared/model/clientes.model.ts
@@ -0,0 +1,4 @@
+import { Cliente } from './cliente.model';
+export class Clientes {
+  clientes: Cliente[];
+}
--- src/app/shared/service/cliente.service.ts
+++ src/app/shared/service/cliente.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Cliente } from '../model/cliente.model';
+import { Clientes } from './../model/clientes.model';
 
 @Injectable({ providedIn: 'root' })
 export class ClienteService {
@@ -14,11 +15,31 @@ export class ClienteService {
 
   constructor(private httpClient: HttpClient) {}
 
+  // Listar Clientes
+  public getClientes(): Observable<Clientes> {
+    return this.httpClient.get<Clientes>(this.apiUrl, this.httpOptions);
+  }
+
+  // Lista Cliente ID
+  // public getClienteWithId(id: string): Observable<Cliente> {
+  //   return this.httpClient.get<Cliente>(
+  //     this.apiUrl + '/' + id,
+  //     this.httpOptions
+  //   );
+  // }
+
+  // Cadastrar Cliente
   public postCliente(cliente: Cliente): Observable<Cliente> {
     return this.httpClient.post<Cliente>(
       this.apiUrl,
       cliente,
       this.httpOptions
     );
+  }
+
+  // Editar Cliente
+  public putCliente(cliente: Cliente): Observable<Cliente> {
+    const body = { nome: cliente.nome, sobrenome: cliente.sobrenome };
+    return this.httpClient.put<Cliente>(this.apiUrl, body, this.httpOptions);
   }
 }
